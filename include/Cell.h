010269c2ecf4181a190906615210c281f4015b01
Martinioini <alemartiale3@gmail.com>
2023-12-21 11:45:12 +0100

    prova

--- include/Cell.h
+++ include/Cell.h
@@ -1,17 +1,35 @@
 #ifndef CELL
 #define CELL.H
 
+#include <string>
 
 class Cell
 {
     private:
-        bool purchased;   //stato di vendita
-        std::string category;     //categoria E, S, L, P, void
-        int house_level;        //livello immobile 0(nessuna casa), 1(casa normale), 2(albergo)
-        int proprietary;        //player propetario
+        
+        bool purchased;          //stato di vendita
+        std::string category;    //categoria E, S, L, P, void
+        int house_level;         //livello immobile 0(nessuna casa), 1(casa normale), 2(albergo)
+        int owner;               //player propetario
         std::string position;
 
     public:
+
+        Cell(int e, int s, int l);
+        
+        bool isPurchased();
+
+        int getOwnerIndex();
+
+        int getHouseLevel();
+
+        std::string getPosition();
+
+        void increaseHouseLevel();
+
+        void deleteOwner();
+
+        void setOwner(int index);
 
 };
 
