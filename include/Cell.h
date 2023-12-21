f1c4c1e751f6dd02fc6439903a2172da096c71cc
zMatty15 <mattiab1409@gmail.com>
2023-12-21 11:01:10 +0100

    First commit

---
+++ include/Cell.h
@@ -0,0 +1,19 @@
+#ifndef CELL
+#define CELL.H
+
+
+class Cell
+{
+    private:
+        bool purchased;   //stato di vendita
+        std::string category;     //categoria E, S, L, P, void
+        int house_level;        //livello immobile 0(nessuna casa), 1(casa normale), 2(albergo)
+        int proprietary;        //player propetario
+        std::string position;
+
+    public:
+
+
+};
+
+#endif CELL
